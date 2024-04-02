7d8499b076569007c2c03f269b9c4f7412e154c8
Matheus Sousa <69647984+SousaMatheus@users.noreply.github.com>
2024-04-02 01:48:13 +0000

    finaliza o endpoint api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,29 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as SUM;");
-  console.log(result.rows);
-  response.status(200).json({ alunos: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+  const getDbVersion = await database.getDbVersion();
+  const dbVersionValue = getDbVersion.rows[0].server_version;
+
+  const maxConnectionsResult = await database.getMaxConnections();
+  const maxConnectionsValue = maxConnectionsResult.rows[0].max_connections;
+
+  var databaseName = process.env.POSTGRES_DB;
+  const currentUsersDbResult =
+    await database.getCurrentUsedConnections(databaseName);
+  const currentUsersDbValue = currentUsersDbResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        current_used_connections: currentUsersDbValue,
+      },
+    },
+  });
+  await database.end;
 }
 
 export default status;
